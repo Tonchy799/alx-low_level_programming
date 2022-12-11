488eaaacc4f5540b6c247a5ba041580c7b3c6c70
Tonchy799 <tonyh799@gmail.com>
2022-12-11 09:53:19 -0800

    4putsc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - main block
- *Return: o
+ * main - Entry
+ * Return: Always 0
  */
 int main(void)
-(
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
