9f8fd478c41254da0f1c91669e548d547a4a6f84
Tonchy799 <tonyh799@gmail.com>
2022-12-16 14:21:46 -0800

    primo

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -12,8 +12,8 @@ int main(void)
 	long int max;
 	long int i;
 
-	n = 612852475143
-	max = -1
+	n = 612852475143;
+	max = -1;
 
 	while (n % 2 == 0)
 	{
@@ -25,7 +25,7 @@ int main(void)
 	{
 		while (n % i == 0)
 		{
-			max = i
+			max = i;
 			n = n / i;
 		}
 	}
