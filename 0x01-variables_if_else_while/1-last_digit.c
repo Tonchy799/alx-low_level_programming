4065c1f254d4a7faba6f01b574b434ef6a4157cc
Tonchy799 <Tonyh799@gmail.com>
2023-12-28 03:43:23 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 5)
-	{printf("Last digit of n is %d: and is greater than 5\n", n); }
+	{printf("Last digit of %d is %d and is greater than 5\n", n, n ); }
 	else
 	{printf("Last digit of n is %d and is 0\n", n); }
 	return (0);
