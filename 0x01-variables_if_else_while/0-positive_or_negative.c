45df2123a9ea506657c60da118c7466a76bf5823
Tonchy799 <Tonyh799@gmail.com>
2023-12-27 23:44:40 -0800

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,10 +15,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	{printf("is positive %d, n\n"); }
+	{printf("is positive\n"); }
 	else if (n < 0)
-	{printf("is negative %d, n\n"); }
+	{printf("is negative\n"); }
 	else
-		printf("is zero %d, n\n"); }
+		printf("is zero\n"); }
 	return (0);
 }
