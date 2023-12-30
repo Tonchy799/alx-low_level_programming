227074faad933029e1e108cadb6307d06a40b709
Tonchy799 <Tonyh799@gmail.com>
2023-12-30 00:32:47 -0800

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * Main - Print the alphabet.
+ * Return - Always 0 (Success)
+ */
+int main(void)
+{
+	char c, d;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{putchar(c);}
+	for (d = 'A'; d <= 'Z'; d++)
+	{putchar(d); }
+	putchar('\n');
+	return (0);
+}
