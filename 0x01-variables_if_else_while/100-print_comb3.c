83efcdc6d54e2f71391699c3d709ab340e376119
Tonchy799 <Tonyh799@gmail.com>
2023-12-30 23:56:59 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,16 +14,16 @@ int main(void)
 	for (d = '0'; d < '9'; d++)
 	{
 		for (p = d + 1; p <= '9'; p++)
-	}
-	if (p != d)
-	{
-		putchar(d);
-		putchar(p);
-		if (d == '8' && p == '9')
 		{
-		continue;
-		putchar(',');
-		putchar(' ');
+			if (p != d)
+			putchar(d);
+			putchar(p);
+			if (d == '8' && p == '9')
+			{
+				continue;
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
