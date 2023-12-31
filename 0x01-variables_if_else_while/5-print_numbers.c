ff16ad32bd3db5524ebfdb119b5fbbe8668563d2
Tonchy799 <Tonyh799@gmail.com>
2023-12-30 23:02:47 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*
+ * main - Entry point.
+ *
+ * Return - Always 0 (Success)
+ */
+int main(void)
+{
+	int u;
+
+	for (u = '0'; u < '10'; u++)
+	{
+		printf("%d\n, u");
+	}
+	return (0);
+}
