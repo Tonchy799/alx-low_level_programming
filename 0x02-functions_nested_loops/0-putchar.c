db5097bed65e205931406ccf3599bcde296bff17
Tonchy799 <Tonyh799@gmail.com>
2024-01-01 03:59:06 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
 /*
- * main - Entry point.
- *
- * Return - 0 (Success)
+ * main - Entry point
+ * Return - Always 0
  */
 int main(void)
 {
-	{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -16,8 +14,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	}
 	_putchar('\n');
 	return (0);
 }
-
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+int _putchar(char c);
+
 void print_alphabet(void);
 
 void print_alphabet_x10(void);
@@ -21,4 +23,3 @@ int add(int, int);
 void print_to_98(int n);
 
 void print_times_table(int n);
-
