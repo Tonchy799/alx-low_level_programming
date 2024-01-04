600a1e1788f34aa966268789f96eedf199f0c8c8
Tonchy799 <Tonyh799@gmail.com>
2024-01-04 13:10:04 -0800

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -5,7 +5,7 @@
  * @a: the array of ints to be reversed
  * @n: the number of elements in the array
  */
-void reverse_array(int *a, int n);
+void reverse_array(int *a, int n)
 {
 	int tmp, index;
 
