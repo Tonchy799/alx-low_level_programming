19cd7ef53c89ecd9990118bb3272dfe39eb9c1c9
Tonchy799 <Tonyh799@gmail.com>
2024-01-04 00:12:57 -0800

    0-multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main -  tests function that prints if an int is positive or negative.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 103;
+	positive_or_negative(i);
+	
+	return (0);
+}
