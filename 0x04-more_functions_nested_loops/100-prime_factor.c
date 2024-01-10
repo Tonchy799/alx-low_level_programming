bf00ba4cbc86f9a51168726b5d0cb24c8ee2bc67
Tonchy799 <Tonyh799@gmail.com>
2024-01-10 02:01:48 -0800

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,19 +6,19 @@
  */
 int main(void)
 {
-	long prime 612852475143, divisor;
+	long prime = 612852475143, divisor;
 
 	while (divisor < (prime / 2))
 	{
 		if ((prime % 2) == 0)
 		{
-			prime /- 2;
+			prime /= 2;
 			continue;
 		}
 		for (divisor = 3; divisor < (prime / 2); divisor += 2)
 		{
 			if ((prime % divisor) == 0)
-				prime  /- divisor;
+				prime /= divisor;
 		}
 	}
 	printf("%ld\n", prime);
