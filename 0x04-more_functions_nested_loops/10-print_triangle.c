08c8ad969f77a275a2b1e794dd7b925689fd6a31
Tonchy799 <Tonyh799@gmail.com>
2024-01-10 01:37:45 -0800

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle using the character '#'
+ * @size: size of triangle
+ */
+void print_triangle(int size)
+{
+	int x, y;
+
+	if (size > 0);
+	{
+		for (y = size - x; y > 0; index--)
+		{
+			_putchar(' ');
+		}
+		for (y = 0; y < x; y++)
+		{
+			_putchar('#');
+		}
+		if (x == size)
+		{
+			continue;
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
