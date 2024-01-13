360179b8e686bb922611b65df6df9ff8063d39b2
Tonchy799 <Tonyh799@gmail.com>
2024-01-13 02:54:05 -0800

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Retun: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i <  argc; i++)
+	{
+		print("%s\n", argv[i]);
+	}
+	return (0);
+}
